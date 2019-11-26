ff9a8a96885d00f129191ac3c0f7813dd965c986
Amyas Merivale <merivale@users.noreply.github.com>
2019-11-26 11:22:02 +0100

    added voting form constraints

--- assets/js/site/votingForm.js
+++ assets/js/site/votingForm.js
@@ -10,9 +10,9 @@ if (votingForm && ordinaryDiv) {
   let presidentCount = 0
   let evptCount = 0
 
-  const ordinaryCheckboxes = Array.from(votingForm.querySelectorAll('checkbox[data-ordinary]'))
-  const presidentCheckboxes = Array.from(votingForm.querySelectorAll('checkbox[data-president]'))
-  const evptCheckboxes = Array.from(votingForm.querySelectorAll('checkbox[data-evpt]'))
+  const ordinaryCheckboxes = Array.from(votingForm.querySelectorAll('input[data-ordinary]'))
+  const presidentCheckboxes = Array.from(votingForm.querySelectorAll('input[data-president]'))
+  const evptCheckboxes = Array.from(votingForm.querySelectorAll('input[data-evpt]'))
 
   ordinaryCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener('change', (e) => {
